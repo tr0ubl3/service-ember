0cb5392658d89ad041824275d613f703d03737f6
tr0ubl3 <mentor.1985@gmail.com>
2014-07-12 21:16:38 +0300

    fixed ember install

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,11 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require_tree .
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require service_ember
+
+// for more details see: http://emberjs.com/guides/application/
+window.ServiceEmber = Ember.Application.create()
